caee3b2a3ee0c565ae3783a33451bd8db288f469
AugustoMarcelo <mrclgst10@gmail.com>
2019-07-21 12:37:38 -0300

    Uploading project

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import logo from '~/assets/logo.png';
+
+import Background from '~/components/Background';
+
+import {
+    Container,
+    Form,
+    FormInput,
+    SubmitButton,
+    SignLink,
+    SignLinkText,
+} from './styles';
+
+export default function SignIn() {
+    return (
+        <Background>
+            <Container>
+                <Image source={logo} />
+
+                <Form>
+                    <FormInput
+                        icon="mail-outline"
+                        keyboardType="email-address"
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Digite seu e-mail"
+                    />
+
+                    <FormInput
+                        icon="lock-outline"
+                        secureTextEntry
+                        placeholder="Digite sua senha"
+                    />
+
+                    <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+                </Form>
+                <SignLink onPress={() => {}}>
+                    <SignLinkText>Criar conta gratuita</SignLinkText>
+                </SignLink>
+            </Container>
+        </Background>
+    );
+}
---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import Background from '~/components/Background';
+
+export default function SignUp() {
+    return <Background />;
+}
