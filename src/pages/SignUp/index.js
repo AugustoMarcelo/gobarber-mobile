6f4ec432abe64356fc62b30ffa2464235fa66173
AugustoMarcelo <mrclgst10@gmail.com>
2019-07-21 12:41:22 -0300

    Styling Sign up page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,53 @@
 import React from 'react';
+import { Image } from 'react-native';
+
+import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
 
-export default function SignUp() {
-    return <Background />;
+import {
+    Container,
+    Form,
+    FormInput,
+    SubmitButton,
+    SignLink,
+    SignLinkText,
+} from './styles';
+
+export default function SignUp({ navigation }) {
+    return (
+        <Background>
+            <Container>
+                <Image source={logo} />
+
+                <Form>
+                    <FormInput
+                        icon="person-outline"
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Nome completo"
+                    />
+
+                    <FormInput
+                        icon="mail-outline"
+                        keyboardType="email-address"
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="Digite seu e-mail"
+                    />
+
+                    <FormInput
+                        icon="lock-outline"
+                        secureTextEntry
+                        placeholder="Digite sua senha"
+                    />
+
+                    <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+                </Form>
+                <SignLink onPress={() => navigation.navigate('SignUp')}>
+                    <SignLinkText>Criar conta gratuita</SignLinkText>
+                </SignLink>
+            </Container>
+        </Background>
+    );
 }
