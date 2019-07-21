822b644214b7797784c9b5ab5364194bc22f2371
AugustoMarcelo <mrclgst10@gmail.com>
2019-07-21 16:50:59 -0300

    Dispatching sign in and sign up

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
+import { signUpRequest } from '~/store/modules/auth/actions';
 
 import {
     Container,
@@ -15,10 +17,20 @@ import {
 } from './styles';
 
 export default function SignUp({ navigation }) {
+    const dispatch = useDispatch();
+
     const emailRef = useRef();
     const passwordRef = useRef();
 
-    function handleSubmit() {}
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const loading = useSelector(state => state.auth.loading);
+
+    function handleSubmit() {
+        dispatch(signUpRequest(name, email, password));
+    }
 
     return (
         <Background>
@@ -32,6 +44,8 @@ export default function SignUp({ navigation }) {
                         autoCapitalize="none"
                         placeholder="Nome completo"
                         onSubmitEditing={() => emailRef.current.focus()}
+                        value={name}
+                        onChangeText={setName}
                     />
 
                     <FormInput
@@ -43,6 +57,8 @@ export default function SignUp({ navigation }) {
                         ref={emailRef}
                         returnKeyType="next"
                         onSubmitEditing={() => passwordRef.current.focus()}
+                        value={email}
+                        onChangeText={setEmail}
                     />
 
                     <FormInput
@@ -51,9 +67,13 @@ export default function SignUp({ navigation }) {
                         placeholder="Digite sua senha"
                         ref={passwordRef}
                         returnKeyType="send"
+                        value={password}
+                        onChangeText={setPassword}
                     />
 
-                    <SubmitButton onPress={handleSubmit}>Acessar</SubmitButton>
+                    <SubmitButton loading={loading} onPress={handleSubmit}>
+                        Criar conta
+                    </SubmitButton>
                 </Form>
                 <SignLink onPress={() => navigation.navigate('SignIn')}>
                     <SignLinkText>Já tenho uma conta</SignLinkText>
