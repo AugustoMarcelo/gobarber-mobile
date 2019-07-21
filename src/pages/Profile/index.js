5cac419c4ea5f4d702107191d2fcac43fe6c6f4b
AugustoMarcelo <mrclgst10@gmail.com>
2019-07-21 17:17:31 -0300

    Setting initial route

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
 
-export default function Dashboard() {
+export default function Profile() {
     return <Background />;
 }
 
-Dashboard.navigationOptions = {
-    tabBarLabel: 'Agendamentos',
+Profile.navigationOptions = {
+    tabBarLabel: 'Meu Perfil',
     tabBarIcon: ({ tintColor }) => (
-        <Icon name="event" size={20} color={tintColor} />
+        <Icon name="person" size={20} color={tintColor} />
     ),
 };
