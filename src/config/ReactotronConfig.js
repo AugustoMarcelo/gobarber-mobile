6159e4ae872af18dce4cb77e2301e19b2e6666a4
AugustoMarcelo <mrclgst10@gmail.com>
2019-07-22 14:21:48 -0300

    Implementing update profile

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -3,7 +3,7 @@ import { reactotronRedux } from 'reactotron-redux';
 import reactotronSaga from 'reactotron-redux-saga';
 
 if (__DEV__) {
-    const tron = Reactotron.configure({ host: '192.168.0.102' })
+    const tron = Reactotron.configure({ host: '10.193.138.159' })
         .useReactNative()
         .use(reactotronRedux())
         .use(reactotronSaga())
