619c26015e460da6d1db3c9566877409e5dc4c79
AugustoMarcelo <mrclgst10@gmail.com>
2019-07-22 14:33:35 -0300

    Implementing logout function

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -4,6 +4,7 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
 import { updateProfileRequest } from '~/store/modules/user/actions';
+import { signOut } from '~/store/modules/auth/actions';
 
 import {
     Container,
@@ -12,6 +13,7 @@ import {
     Form,
     FormInput,
     SubmitButton,
+    LogoutButton,
 } from './styles';
 
 export default function Profile() {
@@ -45,6 +47,10 @@ export default function Profile() {
                 confirmPassword,
             })
         );
+    }
+
+    function handleLogout() {
+        dispatch(signOut());
     }
 
     return (
@@ -113,6 +119,9 @@ export default function Profile() {
                     <SubmitButton onPress={handleSubmit}>
                         Atualizar Perfil
                     </SubmitButton>
+                    <LogoutButton onPress={handleLogout}>
+                        Sair do GoBarber
+                    </LogoutButton>
                 </Form>
             </Container>
         </Background>
