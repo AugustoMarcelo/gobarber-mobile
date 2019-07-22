f573771f06e7ab9a45b1e90b84d64c7d442298d9
AugustoMarcelo <mrclgst10@gmail.com>
2019-07-22 08:43:08 -0300

    Styling appointments screen

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -3,7 +3,7 @@ import { reactotronRedux } from 'reactotron-redux';
 import reactotronSaga from 'reactotron-redux-saga';
 
 if (__DEV__) {
-    const tron = Reactotron.configure({ host: '192.168.0.104' })
+    const tron = Reactotron.configure({ host: '192.168.0.102' })
         .useReactNative()
         .use(reactotronRedux())
         .use(reactotronSaga())
