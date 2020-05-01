85d2fbd278e8cc2715f2dcd4f90531e8131a3e47
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-01 10:49:15 -0300

    Updated component to new DatePicker

--- src/components/DateInput/index.android.js
+++ src/components/DateInput/index.android.js
@@ -1,37 +1,49 @@
-import React, { useMemo } from 'react';
-import { DatePickerAndroid } from 'react-native';
+import React, { useMemo, useState } from 'react';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import { format } from 'date-fns';
 import pt from 'date-fns/locale/pt';
+import PropTypes from 'prop-types';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, DateButton, DateText } from './styles';
 
 export default function DateInput({ date, onChange }) {
-    const dateFormatted = useMemo(
-        () => format(date, "dd 'de' MMMM 'de' yyyy", { locale: pt }),
-        [date]
-    );
+  const [show, setShow] = useState(false);
 
-    async function handleOpenPicker() {
-        const { action, year, month, day } = await DatePickerAndroid.open({
-            mode: 'spinner',
-            date,
-        });
+  const dateFormatted = useMemo(
+    () => format(date, "dd 'de' MMMM 'de' yyyy", { locale: pt }),
+    [date]
+  );
 
-        if (action === DatePickerAndroid.dateSetAction) {
-            const selectedDate = new Date(year, month, day);
+  function handleOpenPicker() {
+    setShow(true);
+  }
 
-            onChange(selectedDate);
-        }
-    }
+  const handleSelectDate = (event, selectedDate) => {
+    setShow(false);
+    onChange(selectedDate || date);
+  };
 
-    return (
-        <Container>
-            <DateButton onPress={handleOpenPicker}>
-                <Icon name="event" color="#fff" size={20} />
-                <DateText>{dateFormatted}</DateText>
-            </DateButton>
-        </Container>
-    );
+  return (
+    <Container>
+      <DateButton onPress={handleOpenPicker}>
+        <Icon name="event" color="#fff" size={20} />
+        <DateText>{dateFormatted}</DateText>
+      </DateButton>
+      {show && (
+        <DateTimePicker
+          value={date}
+          display="spinner"
+          mode="date"
+          onChange={handleSelectDate}
+        />
+      )}
+    </Container>
+  );
 }
+
+DateInput.propTypes = {
+  date: PropTypes.instanceOf(Date),
+  onChange: PropTypes.func,
+};
