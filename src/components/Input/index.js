9228334faca2cdfc2855bd54e6c739eec655aa4d
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-01 15:05:15 -0300

    Removed autocomplete

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -5,24 +5,22 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Container, TInput } from './styles';
 
 function Input({ style, icon, ...rest }, ref) {
-    return (
-        <Container style={style}>
-            {icon && (
-                <Icon name={icon} size={20} color="rgba(255,255,255,0.6)" />
-            )}
-            <TInput {...rest} ref={ref} />
-        </Container>
-    );
+  return (
+    <Container style={style}>
+      {icon && <Icon name={icon} size={20} color="rgba(255,255,255,0.6)" />}
+      <TInput {...rest} ref={ref} autoCompleteType="off" />
+    </Container>
+  );
 }
 
 Input.propTypes = {
-    icon: PropTypes.string,
-    style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  icon: PropTypes.string,
+  style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
 };
 
 Input.defaultProps = {
-    icon: null,
-    style: {},
+  icon: null,
+  style: {},
 };
 
 export default forwardRef(Input);
